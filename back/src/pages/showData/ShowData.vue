75a5e7b2aa087daaae2494b41559ac6523a39e69
Rain-field <1244082815@qq.com>
2019-04-19 23:51:53 +0800

    完成后台首页数据展示页面

--- back/src/pages/showData/ShowData.vue
+++ back/src/pages/showData/ShowData.vue
@@ -9,8 +9,8 @@
     </div>
     <div class="dataNaver">
       <div class="dataItem dataItem2">
-        <div class="itemTop">用户、会员新增趋势</div>
-        <div class="itemBottom" ref="mychart1" id="mychart1"></div>
+        <div class="itemTop">注册用户、会员新增趋势</div>
+        <div class="itemBottom" id="mychart1"></div>
       </div>
       <div class="dataItem dataItem2">
         <div class="itemTop">用户总量分类</div>
@@ -18,13 +18,13 @@
       </div>
     </div>
     <div class="dataNaver">
-      <div class="dataItem dataItem2">
-        <div class="itemTop">总营收</div>
-        <div class="itemBottom"></div>
+      <div class="dataItem dataItem3">
+        <div class="itemTop">商品总营收</div>
+        <div class="itemBottom" id="mychart3"></div>
       </div>
-      <div class="dataItem dataItem2">
+      <div class="dataItem dataItem4">
         <div class="itemTop">营收类目</div>
-        <div class="itemBottom"></div>
+        <div class="itemBottom" id="mychart4"></div>
       </div>
     </div>
   </div>
@@ -40,7 +40,7 @@ export default {
           num: "1000"
         },
         {
-          name: "年度总收入",
+          name: "年度总营收",
           price: "51600000"
         },
         {
@@ -61,8 +61,8 @@ export default {
         tooltip: {},
         color: ["#4ecb73", "#3aa1ff"],
         legend: {
-          data: ["用户", "会员"],
-          left: 'right',
+          data: ["注册用户", "会员"],
+          left: "right"
         },
         grid: {
           left: "3%",
@@ -94,6 +94,7 @@ export default {
           ]
         },
         yAxis: {
+          name: "(个)",
           axisTick: {
             show: false
           },
@@ -103,7 +104,7 @@ export default {
         },
         series: [
           {
-            name: "用户",
+            name: "注册用户",
             type: "line",
             label: {
               show: true
@@ -126,13 +127,13 @@ export default {
       let myChart = this.$echarts.init(document.getElementById("mychart2"));
       myChart.setOption({
         tooltip: {
-          trigger: 'item',
-          formatter: "{a} <br/>{b} : {c} ({d}%)"
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c}个 ({d}%)"
         },
         color: ["#4ecb73", "#3aa1ff"],
         legend: {
           data: ["非会员", "会员"],
-          left: 'right',
+          left: "right"
         },
         grid: {
           left: "3%",
@@ -141,35 +142,177 @@ export default {
           containLabel: true
         },
         series: [
-         {
-            name: '用户总量',
-            type: 'pie',
-            radius : '65%',
-            center: ['50%', '60%'],
-            data:[
-                {value:335, name:'会员'},
-                {value:310, name:'非会员'},
+          {
+            name: "用户总量",
+            type: "pie",
+            radius: "65%",
+            center: ["50%", "60%"],
+            data: [
+              { value: 335, name: "会员" },
+              { value: 310, name: "非会员" }
             ],
             itemStyle: {
-                emphasis: {
-                    shadowBlur: 10,
-                    shadowOffsetX: 0,
-                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                }
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              }
             }
-        }
+          }
+        ]
+      });
+      myChart.resize();
+    },
+    draws3() {
+      let myChart = this.$echarts.init(document.getElementById("mychart3"));
+      myChart.setOption({
+        tooltip: {},
+        color: ["#4ecb73", "#3aa1ff", "#edafda", "#36cbcb"],
+        legend: {
+          data: ["鲜花", "蛋糕", "绿植", "礼品"],
+          left: "right"
+        },
+        grid: {
+          left: "3%",
+          right: "4%",
+          bottom: "0%",
+          containLabel: true
+        },
+        xAxis: {
+          type: "category",
+          axisTick: {
+            show: false
+          },
+          axisLine: {
+            show: false
+          },
+          data: [
+            "1月",
+            "2月",
+            "3月",
+            "4月",
+            "5月",
+            "6月",
+            "7月",
+            "8月",
+            "9月",
+            "10月",
+            "11月",
+            "12月"
+          ]
+        },
+        yAxis: {
+          name: "(元)",
+          axisTick: {
+            show: false
+          },
+          axisLine: {
+            show: false
+          }
+        },
+        series: [
+          {
+            name: "鲜花",
+            type: "bar",
+            label: {
+              show: true
+            },
+            data: [250, 300, 660, 400, 300, 400, 960, 730, 750, 840, 520, 870]
+          },
+          {
+            name: "蛋糕",
+            type: "bar",
+            stack: "总收入",
+            label: {
+              show: true
+            },
+            data: [150, 200, 360, 100, 100, 200, 760, 230, 450, 640, 120, 670]
+          },
+          {
+            name: "绿植",
+            type: "bar",
+            stack: "总收入",
+            label: {
+              show: true
+            },
+            data: [110, 100, 205, 160, 400, 207, 306, 503, 405, 504, 302, 507]
+          },
+          {
+            name: "礼品",
+            type: "bar",
+            stack: "总收入",
+            label: {
+              show: true
+            },
+            data: [520, 220, 160, 300, 400, 100, 106, 205, 705, 601, 202, 107]
+          }
+        ]
+      });
+      myChart.resize();
+    },
+    draws4() {
+      let myChart = this.$echarts.init(document.getElementById("mychart4"));
+      myChart.setOption({
+        color: ["#4ecb73", "#3aa1ff", "#edafda", "#36cbcb"],
+        // tooltip: {
+        //   trigger: "item",
+        //   formatter: "{a} <br/>{b}: {c} ({d}%)"
+        // },
+        legend: {
+          x: "right",
+          data: ["鲜花", "蛋糕", "绿植", "礼品"]
+        },
+        series: [
+          {
+            name: "访问来源",
+            type: "pie",
+            center:['52%','55%'],
+            radius: ["55%", "80%"],
+            avoidLabelOverlap: false,
+            label: {
+              normal: {
+                    show: false,
+                    position: 'center',
+                        formatter:'{b}'+'\n\n'+'{c}元 ,{d}%'
+                },
+              emphasis: {
+                    show: true,
+                    textStyle: {
+                        fontSize: '18',
+                        fontWeight: 'bold'
+                    },
+                }
+            },
+            labelLine: {
+              normal: {
+                show: false
+              }
+            },
+            data: [
+              { value: 335, name: "鲜花" },
+              { value: 310, name: "蛋糕" },
+              { value: 234, name: "绿植" },
+              { value: 135, name: "礼品" }
+            ]
+          }
         ]
       });
       myChart.resize();
     }
   },
   mounted() {
-    this.draws1();
-    this.draws2();
+    setTimeout(() => {
+      this.draws1();
+      this.draws2();
+      this.draws3();
+      this.draws4();
+    });
     window.onresize = () => {
       return (() => {
         this.draws1();
         this.draws2();
+        this.draws3();
+        this.draws4();
       })();
     };
   }
@@ -206,6 +349,18 @@ export default {
       width: 48%;
       .itemBottom {
         height: 300px;
+      }
+    }
+    .dataItem3 {
+      width: 65%;
+      .itemBottom {
+        height: 350px;
+      }
+    }
+    .dataItem4 {
+      width: 33%;
+      .itemBottom {
+        height: 350px;
       }
     }
   }
